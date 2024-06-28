676f2c4b5165d15e609f37153c87d8912bc80f16
James T <github.jtara1@gmail.com>
2024-06-27 22:46:18 -0700

    feat: (wip) trying bubbles theme

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -27,21 +27,68 @@ local function extra_mode_status()
   return ''
 end
 
+-- Bubbles config for lualine
+-- Author: lokesh-krishna
+-- MIT license, see LICENSE for more details.
+
+-- stylua: ignore
+local colors = {
+  blue   = '#80a0ff',
+  cyan   = '#79dac8',
+  black  = '#080808',
+  white  = '#c6c6c6',
+  red    = '#ff5189',
+  violet = '#d183e8',
+  grey   = '#303030',
+}
+
+local bubbles_theme = {
+  normal = {
+    a = { fg = colors.black, bg = colors.violet },
+    b = { fg = colors.white, bg = colors.grey },
+    c = { fg = colors.white },
+  },
+
+  insert = { a = { fg = colors.black, bg = colors.blue } },
+  visual = { a = { fg = colors.black, bg = colors.cyan } },
+  replace = { a = { fg = colors.black, bg = colors.red } },
+
+  inactive = {
+    a = { fg = colors.white, bg = colors.black },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white },
+  },
+}
+
 require('lualine').setup {
   globalstatus = true,
   sections = {
-    lualine_c = {
-      -- nvim-navic
-      { navic.get_location, cond = navic.is_available },
-    },
-    lualine_z = {
-      -- (see above)
-      { extra_mode_status },
-    },
+    -- bubbles theme
+      lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+      lualine_b = { 'filename', 'branch' },
+      lualine_c = {
+        '%=', --[[ add your center compoentnts here in place of this comment ]]
+      },
+      lualine_x = {},
+      lualine_y = { 'filetype', 'progress' },
+      lualine_z = {
+        { 'location', separator = { right = '' }, left_padding = 2 },
+      },
   },
   options = {
-    theme = 'auto',
-  },
+      theme = bubbles_theme,
+      component_separators = '',
+      section_separators = { left = '', right = '' },
+    },
+
+    inactive_sections = {
+      lualine_a = { 'filename' },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = { 'location' },
+    },
   -- Example top tabline configuration (this may clash with other plugins)
   -- tabline = {
   --   lualine_a = {
