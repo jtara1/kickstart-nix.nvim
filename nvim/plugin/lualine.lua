3e0ff118f603fbcbc4a46e513698b63a01567cfc
James T <github.jtara1@gmail.com>
2024-06-29 23:28:06 -0700

    refactor: reverted lualine.lua to original in kickstart-nix.nvim

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -27,68 +27,20 @@ local function extra_mode_status()
     return ''
 end
 
--- Bubbles config for lualine
--- Author: lokesh-krishna
--- MIT license, see LICENSE for more details.
-
--- stylua: ignore
-local colors = {
-    blue = '#80a0ff',
-    cyan = '#79dac8',
-    black = '#080808',
-    white = '#c6c6c6',
-    red = '#ff5189',
-    violet = '#d183e8',
-    grey = '#303030',
-}
-
-local bubbles_theme = {
-    normal = {
-        a = { fg = colors.black, bg = colors.violet },
-        b = { fg = colors.white, bg = colors.grey },
-        c = { fg = colors.white },
-    },
-
-    insert = { a = { fg = colors.black, bg = colors.blue } },
-    visual = { a = { fg = colors.black, bg = colors.cyan } },
-    replace = { a = { fg = colors.black, bg = colors.red } },
-
-    inactive = {
-        a = { fg = colors.white, bg = colors.black },
-        b = { fg = colors.white, bg = colors.black },
-        c = { fg = colors.white },
-    },
-}
-
 require('lualine').setup {
     globalstatus = true,
     sections = {
-        -- bubbles theme
-        lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-        lualine_b = { 'filename', 'branch' },
         lualine_c = {
-            '%=', --[[ add your center compoentnts here in place of this comment ]]
+            -- nvim-navic
+            { navic.get_location, cond = navic.is_available },
         },
-        lualine_x = {},
-        lualine_y = { 'filetype', 'progress' },
         lualine_z = {
-            { 'location', separator = { right = '' }, left_padding = 2 },
+            -- (see above)
+            { extra_mode_status },
         },
     },
     options = {
-        theme = bubbles_theme,
-        component_separators = '',
-        section_separators = { left = '', right = '' },
-        icons_enabled = true,
-    },
-
-    inactive_sections = {
-        lualine_a = { 'filename' },
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = { 'location' },
+        theme = 'auto',
     },
     -- Example top tabline configuration (this may clash with other plugins)
     -- tabline = {
