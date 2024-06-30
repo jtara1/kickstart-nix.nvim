8bb1fbaed646c48074349b327bc55af105ac7e2a
James T <github.jtara1@gmail.com>
2024-06-29 20:45:15 -0700

    feat: added comment plugin

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -71,6 +71,7 @@ with final.pkgs.lib; let
     # ^ navigation/editing enhancement plugins
     # Useful utilities
     nvim-unception # Prevent nested neovim sessions | nvim-unception
+    nvim-treesitter-parsers.comment
     # ^ Useful utilities
     # libraries that other plugins depend on
     sqlite-lua
